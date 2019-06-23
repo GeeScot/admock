0aef1476f551b397f748cfd8c10a24891fed641d
gurparit <github@gurparit.com>
2019-06-23 19:33:49 +0100

    added separate blacklist cache; need to refactor code but suitably fault tolerant now; needs a good refactor;

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func fetchDNSoverTLS(query string) ([]byte, error) {
 
 func isBlacklistDomain(dns dnsmessage.Message) ([]byte, bool) {
 	domain := dns.Questions[0].Name.String()
-	_, found := inMemoryCache.Get(domain)
+	_, found := blacklistCache.Get(domain)
 	if !found {
 		return nil, false
 	}
@@ -54,50 +54,93 @@ func isBlacklistDomain(dns dnsmessage.Message) ([]byte, bool) {
 	fakeDNS := NewFakeDNS(dns.Header.ID, domain)
 	packed, err := fakeDNS.Pack()
 	if err != nil {
-		fmt.Println(err.Error())
-		return nil, false
+		panic(err)
 	}
 
 	return packed, true
 }
 
-func isCachedDomain(encodedQuery string) ([]byte, bool) {
-	cachedItem, found := inMemoryCache.Get(encodedQuery)
+func isCachedDomain(id uint16, domain string) ([]byte, bool) {
+	cachedItem, found := inMemoryCache.Get(domain)
 	if !found {
 		return nil, false
 	}
 
-	cachedDNS := cachedItem.([]byte)
-	return cachedDNS, true
+	cachedDNS := cachedDNSRecord(id, domain, cachedItem)
+	data, _ := cachedDNS.Pack()
+	return data, true
 }
 
-func addToCache(key string, record []byte) {
-	// var m dnsmessage.Message
-	// err := m.Unpack(record)
-	// if err != nil {
-	// 	fmt.Println(err.Error())
-	// 	return
-	// }
+func cachedDNSRecord(id uint16, domain string, resouceBody interface{}) dnsmessage.Message {
+	name := dnsmessage.MustNewName(domain)
+	record := resouceBody.(dnsmessage.ResourceBody)
 
-	// fmt.Println(m)
-	inMemoryCache.Set(key, record, 3600*time.Second)
+	question := dnsmessage.Question{
+		Name:  name,
+		Type:  dnsmessage.TypeA,
+		Class: dnsmessage.ClassINET,
+	}
+	answer := dnsmessage.Resource{
+		Header: dnsmessage.ResourceHeader{
+			Name:  name,
+			Type:  dnsmessage.TypeA,
+			Class: dnsmessage.ClassINET,
+			TTL:   1,
+		},
+		Body: record,
+	}
+
+	dnsRecord := dnsmessage.Message{
+		Header:    dnsmessage.Header{Response: true, ID: id},
+		Questions: []dnsmessage.Question{question},
+		Answers:   []dnsmessage.Resource{answer},
+	}
+
+	return dnsRecord
 }
 
+func addToCache(record []byte) {
+	var m dnsmessage.Message
+	err := m.Unpack(record)
+	if err != nil {
+		panic(err)
+	}
+
+	domain := m.Questions[0].Name.String()
+	if len(m.Answers) <= 0 {
+		return
+	}
+
+	value := m.Answers[0].Body
+	ttl := m.Answers[0].Header.TTL
+
+	inMemoryCache.Set(domain, value, time.Duration(ttl)*time.Second)
+}
+
+var blacklistCache *cache.Cache
 var inMemoryCache *cache.Cache
 
+func dontPanic() {
+	if r := recover(); r != nil {
+		fmt.Println("[recovered] ", r)
+	}
+}
+
 func main() {
+	blacklistCache = cache.New(3600*time.Minute, 10*time.Minute)
 	inMemoryCache = cache.New(3600*time.Minute, 10*time.Minute)
 
 	conn, _ := net.ListenUDP("udp", &net.UDPAddr{Port: 53})
 	defer conn.Close()
 
-	LoadBlacklists(inMemoryCache)
+	LoadBlacklists(blacklistCache)
 
 	for {
+		defer dontPanic()
+
 		dns, encodedQuery, addr, err := waitForDNS(conn)
 		if err != nil {
-			fmt.Println(err.Error())
-			continue
+			panic(err)
 		}
 
 		if fakeDNS, blacklisted := isBlacklistDomain(*dns); blacklisted {
@@ -105,13 +148,13 @@ func main() {
 			continue
 		}
 
-		if cachedDNS, cached := isCachedDomain(encodedQuery); cached {
+		if cachedDNS, cached := isCachedDomain(dns.Header.ID, dns.Questions[0].Name.String()); cached {
 			conn.WriteToUDP(cachedDNS, addr)
 			continue
 		}
 
 		result, err := fetchDNSoverTLS(encodedQuery)
-		addToCache(encodedQuery, result)
+		addToCache(result)
 		conn.WriteToUDP(result, addr)
 	}
 }
